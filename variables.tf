8b6c5edfd72882f3b32fea7d97847189994d7a12
madan <madanmohan7793@gmail.com>
2022-07-18 15:23:31 +0530

    nothing

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  type    = string
+  default = "prod"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "s3_bucket" {
+  type    = string
+  default = "test"
+}
